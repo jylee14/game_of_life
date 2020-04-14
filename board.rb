a2a88304762e8302d48bd98118d0e3869186f2f8
Jun Lee <jy45lee@gmail.com>
2020-04-13 22:40:27 -0700

    testing simple parts of program

--- board.rb
+++ board.rb
@@ -4,7 +4,11 @@ class Board
     attr_reader :game_board
 
     def initialize n
-        return nil unless n.is_a?(Numeric) && n > 3 
+        unless n.is_a?(Numeric) && n > 2 
+            @game_board = nil
+            return nil
+        end 
+
         coin = Random.new 
         @board_size = n
         @game_board = Array.new(n) {
@@ -18,13 +22,11 @@ class Board
         @game_board
     end 
 
+    # simple implementation of printing out a 2x2 array 
     def print_board 
-        system "clear"
-        (5*@board_size).times{
-            print "-"
-        }
-        print "\n"
-
+        system "clear"       
+        
+        print_bounds
         @board_size.times{ |i| 
             print "|   "
             @board_size.times{ |j| 
@@ -36,8 +38,59 @@ class Board
                 puts
             end
         }
+        print_bounds
+    end 
+    
+    # main logic for Game of Life 
+    # will follow the following rules 
+    #   1. Any live cell with two or three live neighbors survives.
+    #   2. Any dead cell with three live neighbors becomes a live cell.
+    #   3. All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+    # and print the resulting board 
+    def advance
+        old = snapshot
+        self.print_board
+    end 
+
+    #:private
+    # create a deep copy of the current board to act as a reference snapshot to calculate 
+    # the next tick
+    def snapshot 
+        snap = []
+        @game_board.each{ |row|
+            row_copy = [] 
+            row.each{|val| row_copy << val}
+            snap << row_copy
+        }
+        snap
+    end 
+
+    # Check to see if the cell at given position will become live or die 
+    # will check its 8 neighbors (less for boundary cells)
+    def check_liveliness(i,j)
+        left_bound = i-1 unless i == 0
+        right_bound = i+1 unless i == @board_size - 1
+        upper_bound = j-1 unless j == 0
+        lower_bound = j+1 unless j == @board_size - 1
+
+        neighbors = [] 
+        (left_bound..right_bound).each{ |row|
+            (upper_bound..lower_bound).each{ |col| 
+                neighbors << @game_board[row][col] unless row == i && col == j
+            }
+        }
+        live_neighbors = neighbors.count{ |x| x == 1 }
+        if @game_board[i][j] == 0 
+            live_neighbors == 3 
+        else 
+            live_neighbors == 2 || live_neighbors == 3 
+        end 
+    end 
+
+    def print_bounds 
         (5*@board_size).times{
             print "-"
         }
+        puts
     end 
 end 
--- tc_board_simple.rb
+++ tc_board_simple.rb
@@ -4,20 +4,26 @@ include Test::Unit::Assertions
 require_relative 'board'
 
 class Board_Tests < Test::Unit::TestCase
-    def board_test0
-        puts "starting board_test0"
-        assert_nil( board.new 'a')
-        puts "passed board_test0"
+    def test_failing_board_init
+        assert_nil Board.new('a').game_board
+        assert_nil Board.new(2).game_board
     end 
 
-    def board_test1
-        board1 = Board.new(2).game_board #a 2x2 board 
+    def test_board_init_with_0_or_1
+        board1 = Board.new(3).game_board #a 3x3 board 
         board1.each{ |row| 
             row.each{ |val|
                 assert (val == 0 || val == 1)
             }
         }
-        assert false
-        puts "Passed board_test1"
+    end 
+
+    def test_snapshot
+        (3..10).each{ |n| 
+            board = Board.new(n)
+    
+            old = board.snapshot
+            assert_equal board.game_board, old
+        }
     end 
 end 
